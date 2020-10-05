03b0d0dabe53bcab0c360b2cfd3a743da2313dff
pkasemer <pkasemer@gmail.com>
2020-10-05 13:10:21 +0300

    inititial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDmWPin8oBUnooEuk5HgWaV-kPl2pWs0Hw",
+    authDomain: "zodongo-foods.firebaseapp.com",
+    databaseURL: "https://zodongo-foods.firebaseio.com",
+    projectId: "zodongo-foods",
+    storageBucket: "zodongo-foods.appspot.com",
+    messagingSenderId: "284397086378",
+    appId: "1:284397086378:web:15bad9975f464f8b6952d3",
+    measurementId: "G-CJ4L5Y4FVJ"
+  };
